1c67a5f369111759a3cdc2c8f10923232109d6ae
Justus Omondi <85983194+Justus446@users.noreply.github.com>
2023-09-08 19:06:53 +0300

    Initialize cpara_pbd_bench3

---
+++ cpovc_pfs/urls.py
@@ -0,0 +1,25 @@
+"""Preventing and Family support app urls."""
+from django.urls import path, re_path
+from . import views
+
+urlpatterns = [
+    path('', views.pfs_home, name='pfs_home'),
+    path('new/<int:id>/', views.new_pfs, name='new_pfs'),
+    path('view/<int:id>/', views.view_pfs, name='view_pfs'),
+    path('edit/<int:id>/', views.edit_pfs, name='edit_pfs'),
+
+    # Preventive Registers
+    path('preventiveattendanceregister/new/<int:id>/',
+         views.preventive_attendance_register,
+         name='preventiveattendanceregister'),
+    path('preventiveattendanceregister/save/',
+         views.save_preventive_register, name='save_preventive_register'),
+    path('preventiveattendanceregister/manage/',
+         views.manage_preventive_register, name='manage_preventive_register'),
+    re_path('preventiveattendanceregister/delete/(?P<id>\d+)/(?P<btn_event_type>\w+)/(?P<btn_event_pk>.+)/', views.delete_preventive_event_entry,
+            name='delete_preventive_event_entry'),
+    re_path('preventiveattendanceregister/edit/(?P<id>\d+)/(?P<btn_event_type>\w+)/(?P<btn_event_pk>.+)/', views.edit_preventive_event_entry,
+            name='edit_preventive_event_entry'),
+    # Version 2 Thinking
+    path('register/new/<int:id>/', views.new_register_v2, name='new_register'),
+]
